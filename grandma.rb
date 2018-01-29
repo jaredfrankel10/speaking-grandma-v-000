21b128747bcdcabd471bc0a9f716c5c507667f1a
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 16:56:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
    input = phrase
- 
+
    if phrase == "I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == input.upcase
